371f4b8fa86ef61c859de099a64041ac09bf1d81
109kms <109kms@naver.com>
2025-12-20 12:52:42 +0900

    User entity에 정적 팩토리 메소드 추가

--- backend/ticket-service/src/main/java/com/ticket/user/entity/User.java
+++ backend/ticket-service/src/main/java/com/ticket/user/entity/User.java
@@ -17,10 +17,26 @@ public class User {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   
+  @Column(nullable = false, unique = true)
   private String email;
   
+  @Column(nullable = false)
   private String password;
   
+  @Column(nullable = false)
   private String name;
+  
+  @Column(nullable = false)
+  private Long point = 0L;
+  
+  // 정적 팩토리 메소드
+  public static User create(String email, String password, String name) {
+    User u = new User();
+    u.email = email;
+    u.password = password;
+    u.name = name;
+    u.point = 0L;
+    return u;
+  }
   
 }
