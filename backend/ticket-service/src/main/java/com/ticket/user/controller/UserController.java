060c09fa2c4fa2f3cfe3aa3749ce2660692dccfa
109kms <109kms@naver.com>
2025-12-20 15:40:30 +0900

    UserController - PostMapping 추가

--- backend/ticket-service/src/main/java/com/ticket/user/controller/UserController.java
+++ backend/ticket-service/src/main/java/com/ticket/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.ticket.user.controller;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,6 +21,7 @@ public class UserController {
 
   private final UserService userService;
   
+  @PostMapping("/signup")
   public ResponseEntity<UserResponseDTO> signup(@Valid @RequestBody SignupRequestDTO requestDTO) {
     UserResponseDTO responseDTO = userService.signup(requestDTO);
     return ResponseEntity.status(HttpStatus.CREATED).body(responseDTO);
