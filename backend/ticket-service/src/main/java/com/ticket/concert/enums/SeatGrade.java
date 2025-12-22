150283e8ebce4eaa0f2b303463548ba11c937695
109kms <109kms@naver.com>
2025-12-22 15:22:14 +0900

    SeatGrade, SeatStatus Enum 추가

---
+++ backend/ticket-service/src/main/java/com/ticket/concert/enums/SeatGrade.java
@@ -0,0 +1,5 @@
+package com.ticket.concert.enums;
+
+public enum SeatGrade {
+  VIP, R, S
+}
---
+++ backend/ticket-service/src/main/java/com/ticket/concert/enums/SeatStatus.java
@@ -0,0 +1,7 @@
+package com.ticket.concert.enums;
+
+public enum SeatStatus {
+  AVAILABLE,            // 예약 가능
+  TEMPORARY_RESERVED,   // 예약 진행중 (일시 선점)
+  SOLD                  // 판매 완료
+}
