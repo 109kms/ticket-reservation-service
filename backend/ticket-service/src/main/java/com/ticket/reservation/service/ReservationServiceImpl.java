ef1fe8da0df8bc45161ab65192aa84e49747a47f
109kms <109kms@naver.com>
2025-12-23 14:15:52 +0900

    Seat - 좌석 확정처리하는 메소드 추가 / ReservationService, ReservationServiceImpl - 예약 생성 및 저장 비즈니스 로직 구현

---
+++ backend/ticket-service/src/main/java/com/ticket/reservation/service/ReservationServiceImpl.java
@@ -0,0 +1,45 @@
+package com.ticket.reservation.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.ticket.concert.entity.Seat;
+import com.ticket.concert.repository.SeatRepository;
+import com.ticket.reservation.dto.request.ReservationRequestDTO;
+import com.ticket.reservation.dto.response.ReservationResponseDTO;
+import com.ticket.reservation.entity.Reservation;
+import com.ticket.reservation.repository.ReservationRepository;
+import com.ticket.user.entity.User;
+import com.ticket.user.repository.UserRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ReservationServiceImpl implements ReservationService {
+  
+  private final ReservationRepository reservationRepository;
+  private final UserRepository userRepository;
+  private final SeatRepository seatRepository;
+
+  @Override
+  @Transactional
+  public ReservationResponseDTO makeReservation(ReservationRequestDTO requestDTO) {
+    
+    User user = userRepository.findById(requestDTO.getUserId())
+        .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 사용자입니다."));
+    
+    Seat seat = seatRepository.findById(requestDTO.getSeatId())
+        .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 좌석입니다."));
+    
+    seat.confirm(); // 좌석 확정처리
+    
+    Reservation reservation = Reservation.create(user, seat);  // 예약 엔티티 생성
+    
+    Reservation saveReservation = reservationRepository.save(reservation);  // 저장
+    
+    return ReservationResponseDTO.from(saveReservation);  // dto로 변환
+  }
+
+}
